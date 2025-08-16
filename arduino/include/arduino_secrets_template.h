c40f7fda5587b2b8ead6021d1e5bbd1eb0fec187
youngpapipap <marvin.back@t-online.de>
2025-08-16 14:15:02 +0200

    add exception in .gitignore so arduino_secrets_template.h is tracked

---
+++ arduino/include/arduino_secrets_template.h
@@ -0,0 +1,7 @@
+#define ROOM_ID 1
+#define TOPIC "topic/subtopic" //Sensortype is automatically added
+#define SECRET_SSID "wifi ssid"
+#define SECRET_PASS "wifi password"
+#define BROKER "broker address"
+#define MQTT_USER "username"
+#define MQTT_PASS "userpassword"
