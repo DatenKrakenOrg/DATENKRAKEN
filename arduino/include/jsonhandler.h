7b47f5db0f66362bb5bb769b06aee498fca12665
youngpapipap <marvin.back@t-online.de>
2025-08-16 16:25:34 +0200

    Fix:

--- arduino/include/jsonhandler.h
+++ arduino/include/jsonhandler.h
@@ -1,7 +1,6 @@
 #ifndef JSONHANDLER_H 
 #define JSONHANDLER_H
-#include <ArduinoJson.h>
-extern StaticJsonDocument<512> doc;
+
 
 
 #define JSONSIZE 8192
---
+++ frontend/tests/conftest.py
@@ -0,0 +1,8 @@
+import os
+import sys
+
+ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
+if ROOT not in sys.path:
+    sys.path.insert(0, ROOT)
+
+os.environ.setdefault("WEATHER_API_KEY", "TEST_DUMMY_KEY")
