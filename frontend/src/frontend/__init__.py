40695a396657a11d90ee2f266165d53d44da9c44
ReySKR <stephanr.tin23@student.dhbw-heidenheim.de>
2025-08-21 10:45:57 +0200

    refactor: remove testing functions

--- frontend/src/frontend/__init__.py
+++ frontend/src/frontend/__init__.py
@@ -1,18 +1,6 @@
 import os
-from database.orm import SensorType
-from datetime import datetime
 
 if os.getenv("ENVIRONMENT") != "PROD":
     from dotenv import load_dotenv
     load_dotenv()
 
-from utility.datafetcher import DataFetcher
-print(os.getenv("DB_HOST"))
-df = DataFetcher().get_bucket_by_t_interval(
-    SensorType.TEMPERATURE,
-    "401",
-    datetime(2024, 8, 5),
-    datetime.now())
-
-print(df["bucket_time"].min())
-print(df["bucket_time"].max())
