f217f70fe213e4bef99e0d8afb7d376d46e3b3b1
GeroldJ <geroldj.tin23@student.dhbw-heidenheim.de>
2025-09-29 11:53:11 +0200

    dummy daten

--- database/utils/fill_dummy.sql
+++ database/utils/fill_dummy.sql
@@ -3,33 +3,34 @@
 -- Noise script for around 5m entries
 INSERT INTO bronze.noise (time, arduino_id, noise)
 SELECT
-    base_ts + (offset_sec || ' seconds')::interval,
+    date_trunc('second', base_ts + make_interval(secs => offset_sec)),
     CASE
-        WHEN n <= 10 THEN '401'
-        WHEN n > 10 AND n <= 20 THEN '402'
-        ELSE '403'
+        WHEN n <= 10 THEN 401
+        WHEN n > 10 AND n <= 20 THEN 402
+        ELSE 403
     END,
     floor(random() * 1029)::smallint
 FROM
     generate_series(
-        NOW() - '60 days'::interval,
+        NOW() - interval '60 days',
         NOW(),
-        '1 minute'::interval
+        interval '1 minute'
     ) AS timestamps(base_ts)
 CROSS JOIN
     generate_series(0, 1) AS time_offsets(offset_sec)
 CROSS JOIN
     generate_series(1, 30) AS data_points(n);
 
+
 -- Temperature skript for 172k
 
 INSERT INTO bronze.temperature (time, arduino_id, temperature)
 SELECT
-    ts,
+    date_trunc('second', ts),
     CASE
-        WHEN (row_num % 3) = 1 THEN '401'
-        WHEN (row_num % 3) = 2 THEN '402'
-        ELSE '403' 
+        WHEN (row_num % 3) = 1 THEN 401
+        WHEN (row_num % 3) = 2 THEN 402
+        ELSE 403
     END,
     random() * 25.0 + 5.0
 FROM (
@@ -38,8 +39,54 @@ FROM (
         row_number() OVER (ORDER BY ts) as row_num
     FROM
         generate_series(
-            NOW() - '60 days'::interval,
+            NOW() - interval '60 days',
             NOW(),
-            '30 seconds'::interval
+            interval '30 seconds'
+        ) AS timestamps(ts)
+) AS generated_series;
+
+-- humidity skript for 172k
+
+INSERT INTO bronze.humidity (time, arduino_id, humidity)
+SELECT
+    date_trunc('second', ts),
+    CASE
+        WHEN (row_num % 3) = 1 THEN 401
+        WHEN (row_num % 3) = 2 THEN 402
+        ELSE 403
+    END,
+    random() * 25.0 + 5.0
+FROM (
+    SELECT
+        ts,
+        row_number() OVER (ORDER BY ts) as row_num
+    FROM
+        generate_series(
+            NOW() - interval '60 days',
+            NOW(),
+            interval '30 seconds'
+        ) AS timestamps(ts)
+) AS generated_series;
+
+-- voc skript for 172k
+
+INSERT INTO bronze.voc (time, arduino_id, voc)
+SELECT
+    date_trunc('second', ts),
+    CASE
+        WHEN (row_num % 3) = 1 THEN 401
+        WHEN (row_num % 3) = 2 THEN 402
+        ELSE 403
+    END,
+    random() * 25.0 + 5.0
+FROM (
+    SELECT
+        ts,
+        row_number() OVER (ORDER BY ts) as row_num
+    FROM
+        generate_series(
+            NOW() - interval '60 days',
+            NOW(),
+            interval '30 seconds'
         ) AS timestamps(ts)
 ) AS generated_series;
